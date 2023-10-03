5c7e514322083c75cc87ae5c6169c51926fd59d5
Adrien Schroedel <adrien.schroedel91@gmail.com>
2023-10-03 14:54:51 +0200

    Added toString to Hthcard object

--- src/Entity/Hthcard.php
+++ src/Entity/Hthcard.php
@@ -30,6 +30,11 @@ class Hthcard
     
     private ?HearthstoneCardbook $hearthstoneCardbook = null;
 
+
+    public function __toString(){
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
