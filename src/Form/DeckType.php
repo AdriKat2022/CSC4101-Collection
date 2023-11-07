4e3bdaba5876e81ca14fe469f530292925ddc948
Adrien Schroedel <adrien.schroedel91@gmail.com>
2023-11-07 03:04:27 +0100

    can now add cards to decks (and the correct ones)

--- src/Form/DeckType.php
+++ src/Form/DeckType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Deck;
+use App\Entity\Hthcard;
+use App\Repository\HthcardRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,13 +13,34 @@ class DeckType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
+        $deck = $options['data'] ?? null;
+        $member = $deck->getMember();
+
+
         $builder
             ->add('member', null, [
                 'disabled'   => true,
         ])
             ->add('description')
             ->add('public')
-            //->add('cards')
+            ->add('cards', null, [
+                'query_builder' => function (HthcardRepository $er) use ($member) {
+                                return $er->createQueryBuilder('o')
+                                ->leftJoin('o.hearthstoneCardbook', 'i')
+                                ->andWhere('i.member = :member')
+                                ->setParameter('member', $member)
+                                ;
+                        },
+                // avec 'by_reference' => false, sauvegarde les modifications
+                'by_reference' => false,
+                // classe pas obligatoire
+                //'class' => [Object]::class,
+                // permet sélection multiple
+                'multiple' => true,
+                // affiche sous forme de checkboxes
+                'expanded' => true
+            ])
         ;
     }
 
